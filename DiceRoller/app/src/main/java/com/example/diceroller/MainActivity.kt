2033b273cfe9e8fffd0cf941a01d58941001de5f
K M H Mubin <kmh.mubin@northsouth.edu>
2021-02-28 22:53:09 +0600

    project #3: add a toast message when button pressed

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        assign the button ids in a button variable
+        val rollButton: Button = findViewById(R.id.button)
+//        add an event listener for click
+        rollButton.setOnClickListener {
+//            when button pressed it will show this toast message
+            val toast = Toast.makeText(this, "Dice Rolled!!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
