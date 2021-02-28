50140f328a714b125a0d4204566de4a63dac69eb
K M H Mubin <kmh.mubin@northsouth.edu>
2021-02-28 23:20:37 +0600

    project #3: clean up code & add comment

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,26 +5,31 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-// assign a variable for the button
+        // assign a variable for the button
         val rollButton: Button = findViewById(R.id.button)
-//        add an event listener for the button
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        // add an event listener for the button
+        rollButton.setOnClickListener { rollDice() }
     }
 
+/*
+
+Roll the dice and update the screen with the result.
+ */
+
     private fun rollDice() {
-//        assign the dice sides
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-//       save the result in the variable when dice roll
         val diceRoll = dice.roll()
-//        display the result in the textview
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
-//        convert the result in to string
         resultTextView.text = diceRoll.toString()
 
     }
@@ -33,6 +38,7 @@ class MainActivity : AppCompatActivity() {
 // Dice Class
 
 class Dice(private val numSides: Int) {
+    // return random value based on Dice side number
     fun roll(): Int {
         return (1..numSides).random()
     }
