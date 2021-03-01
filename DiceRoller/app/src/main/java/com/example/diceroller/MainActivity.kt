275c3c532b3764d13eb1c9022d876d1731ab4e7f
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-01 20:06:41 +0600

    project #3: clean up the code & add content description

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         // add an event listener for the button
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
 /*
@@ -28,19 +30,24 @@ Roll the dice and update the screen with the result.
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // Update the screen with the dice roll images
+
+        // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
 
-        // set dice images with numbers
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
-
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
 
     }
 }
