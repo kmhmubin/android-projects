98c03a82040a7b71014925c07ed00a063191531a
K M H Mubin <kmh.mubin@northsouth.edu>
2021-03-14 16:48:54 +0600

    project #7: add a calendar date picker and show the selected date

--- AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.ageinminutes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get reference to button
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "button works", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    // calender Date picker function
+
+    fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        //open date picker popup
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                // assign the user input in the variable and format the view
+                // date will show -> 10/2/2021
+                val selectedDate = "$selectedDayOfMonth/ ${selectedMonth + 1}/ $selectedYear"
+
+                // get the reference of the text view
+                val showSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+                showSelectedDate.setText(selectedDate).toString()
+            },
+            year,
+            month,
+            day
+        ).show()
     }
 }
