3c4660e98902f2ba31e32cb812c1c546734d26db
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-04 23:44:14 +0600

    feat #9: add a view holder

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.affirmations.adapter
 
-class ItemAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+
+import com.example.affirmations.model.Affirmation
+
+//    pass the list of affirmations into the adapter and
+// context object instance that pass into an ItemAdapter instance.
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    // creating view holder for ItemAdapter
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
