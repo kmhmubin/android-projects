13cb7410367c3827a0d642be2645ddc74adf2f6b
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-04 21:13:28 +0600

    feat #9: show total lists of text from Data source package

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // add reference link with the text view
+        val textView: TextView = findViewById(R.id.textview)
+
+        // get the total list from affirmation data source and call loadAffirmations() function
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
