628c953d3b3be0e9ce664ce833cfa81b66dce2fa
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-06 20:51:38 +0600

    style #12: add card view input and hide status bar

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
     }
 }
