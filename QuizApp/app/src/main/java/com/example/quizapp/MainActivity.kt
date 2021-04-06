e33733a77982cfd9e5f756ed3d0fd5f341e6488c
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-06 21:27:00 +0600

    feat #12: add question activity and add new data package
    when start button pressed go to new question activity

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,21 @@ class MainActivity : AppCompatActivity() {
 // Remember that you should never show the action bar if the
 // status bar is hidden, so hide that too if necessary.
         actionBar?.hide()
+
+        // when start pressed go to question page
+        val buttonStart: Button = findViewById(R.id.btn_start)
+        buttonStart.setOnClickListener {
+            val enterName: EditText = findViewById(R.id.et_name)
+            if (enterName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                // after inter name got to quiz question page
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                // start new activity and finish this activity
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
