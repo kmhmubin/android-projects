f4ee8c7f92da974df2750b1fcbbb1351572ce956
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-06 21:51:38 +0600

    feat #12: checking the question list size

--- QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -2,10 +2,16 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.quizapp.data.Constants
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        // get the question from constants
+        val questionList = Constants.getQuestions()
+        Log.i("Question size", "${questionList.size}")
     }
 }
--- QuizApp/app/src/main/java/com/example/quizapp/data/Question.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/data/Question.kt
@@ -1,4 +1,12 @@
 package com.example.quizapp.data
 
-class Question {
-}
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
