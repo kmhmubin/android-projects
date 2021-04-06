c88697973702a47efed105daac47647e778bcff4
K M H Mubin <kmh.mubin@northsouth.edu>
2021-04-06 16:07:15 +0600

    feat #11: show the list

--- RecycleViewListApp/app/src/main/java/com/example/recycleviewlistapp/MainActivity.kt
+++ RecycleViewListApp/app/src/main/java/com/example/recycleviewlistapp/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.recycleviewlistapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleviewlistapp.adapter.CardAdapter
 
 class MainActivity : AppCompatActivity() {
+    // creating objects
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var cardAdapter: CardAdapter
+
+    // creating the list
+    private var countryList = mutableListOf<String>()
+    private var displayList = mutableListOf<String>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // creating a list
+        countryList.add("Bangladesh")
+        countryList.add("Brazil")
+        countryList.add("Bhutan")
+        countryList.add("Nepal")
+        countryList.add("India")
+        countryList.add("United State")
+        countryList.add("United Kingdom")
+        countryList.add("United Arab Emirath")
+        countryList.add("Norway")
+        countryList.add("Japan")
+        countryList.add("China")
+
+        // display the list
+
+        displayList.addAll(countryList)
+
+        // show the list into recycler view
+        recyclerView = findViewById(R.id.recyclerView)
+        // show the card adapter
+        cardAdapter = CardAdapter(displayList)
+
+        recyclerView.adapter = cardAdapter
     }
 }
